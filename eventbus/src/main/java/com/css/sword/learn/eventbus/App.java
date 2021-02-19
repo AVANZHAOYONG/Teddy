a4d1644ad9ba3b8233a91e65d250e178dd55bf8f
AVANZHAOYONG <yx1148812692@163.com>
2021-02-19 13:50:02 +0800

    head first 设计模式学习

--- eventbus/src/main/java/com/css/sword/learn/eventbus/App.java
+++ eventbus/src/main/java/com/css/sword/learn/eventbus/App.java
@@ -1,28 +1,23 @@
 package com.css.sword.learn.eventbus;
 
 import com.css.sword.learn.eventbus.event.EtcdChangeEvent;
-import com.css.sword.learn.eventbus.eventBus.EventBus;
 import com.css.sword.learn.eventbus.eventBus.EtcdEventBusManager;
+import com.css.sword.learn.eventbus.eventBus.EventBus;
 import com.css.sword.learn.eventbus.subscriber.EtcdSubscriberOne;
 
 /**
  * Hello world!
  *
+ * @author zhaoyong
  */
-public class App 
-{
-    public static void main( String[] args ) throws InterruptedException
-    {
+public class App {
+    public static void main(String[] args) {
         EventBus eventBus = EtcdEventBusManager.get();
         eventBus.register(new EtcdSubscriberOne());
         EtcdChangeEvent event = new EtcdChangeEvent();
         event.setMessage("This is a message");
         eventBus.post(event);
         System.out.println("Thread-main is over ");
-        
         EtcdEventBusManager.close();
-        
-    	
     }
-    
 }
